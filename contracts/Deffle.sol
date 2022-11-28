b100944cd8ffb47ae60be494858d38bb7aeebea6
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-28 01:27:58 +0100

    feat: add createRaffle function

--- contracts/Deffle.sol
+++ contracts/Deffle.sol
@@ -5,6 +5,61 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
+error Error__CreateRaffle();
 contract Deffle{
+    event Deffle__RaffleCreated(uint id, address indexed raffleOwner);
+
+    enum RaffleState{
+        Open,
+        Calculating,
+        Closed
+    }
+
+    struct Raffle{
+        bytes32 raffleData;
+        uint256 entranceFee;
+        uint256 deadline;
+        uint256 startTime;
+        uint8 maxTickets;
+        address payable[] participants;
+        address payable owner;
+        RaffleState raffleState;
+        uint256 balance;
+    }
+
+    //a mapping of id to Raffles
+    mapping(uint256 => Raffle) public idToRaffle;
+    //an array of all ids
+    uint256[] public idList;
+
+    uint256 id;
+    address payable public owner;
+    uint256 public creationFee;
+
+    constructor(uint256 _creationFee){
+        owner = payable(msg.sender);
+        creationFee = _creationFee;
+    }
     
+
+    function createRaffle(bytes32 _raffleData,
+    uint256 _entranceFee,
+    uint256 _deadline,
+    uint8 _maxTickets ) external payable{
+        if(msg.value < creationFee){
+            revert Error__CreateRaffle();
+        }
+        //Update the mapping with inputted data
+        id++;
+        idToRaffle[id].raffleData = _raffleData;
+        idToRaffle[id].entranceFee = _entranceFee;
+        idToRaffle[id].deadline = _deadline;
+        idToRaffle[id].startTime = block.timestamp;
+        idToRaffle[id].maxTickets = _maxTickets;
+        idToRaffle[id].owner = payable(msg.sender);
+        idToRaffle[id].raffleState= RaffleState.Open;
+
+        emit Deffle__RaffleCreated(id, msg.sender); 
+
+    }
 }
