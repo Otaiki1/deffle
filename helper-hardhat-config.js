86cb6a0d83644e96ddc3ffd35a14f0d2c0233984
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-30 06:01:05 +0100

    feat: add deploy mock scripts

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,3 +1,9 @@
 const networkConfig = {
-    
+    //add config for testNet networks chainId: {name: ,vrfCoordinatorV2: }
+}
+
+const developmentChains = ["hardhat", "localhost"];
+module.exports = {
+    networkConfig,
+    developmentChains,
 }
