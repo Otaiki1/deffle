a50e65cd0e3b2b09c01ff24d41b0f5a585721aaf
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-30 06:51:19 +0100

    refac: configure deploy scripts

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,5 +1,14 @@
+const { ethers } = require("hardhat");
+
 const networkConfig = {
     //add config for testNet networks chainId: {name: ,vrfCoordinatorV2: }
+    31337:{
+        name: "hardhat",
+        creationFee: ethers.utils.parseEther("0.1"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",//30gwei
+        callbackGasLimit: "500000",
+        feePercent: "10"
+    }
 }
 
 const developmentChains = ["hardhat", "localhost"];
